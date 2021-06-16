d801d82003c396121762e7df26c547137183d232
Andros Fenollosa <andros@fenollosa.email>
2021-06-16 10:51:31 +0200

    Add push

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from random import choice
+import os
+
+# Variables
+TELEGRAM_BOT_TOKEN = os.environ['secret.TELEGRAM_BOT_TOKEN']
+TELEGRAM_CHANNEL_ID = os.environ['secret.TELEGRAM_CHANNEL_ID']
+TELEGRAM_URL_SEND = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
 
 # Lee el fichero con las frases
 archivo_con_frases = open("frases.txt", "r")
@@ -9,6 +15,7 @@ archivo_con_frases = open("frases.txt", "r")
 lineas = archivo_con_frases.readlines()
 # Limpia los saltos de línea
 frases = tuple(map(lambda linea: linea.strip(), lineas))
-
-
-print(choice(frases))
+# Obtiene frase aleatoria
+frase_aleatoria = choice(frases)
+# Envía al canal
+os.system(f"curl -s -X POST https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id={TELEGRAM_CHANNEL_ID} -d text='{frase_aleatoria}'")
